cb11ef09b98ec01298e579e2e230e40387aa7dd2
MehdiChaouche <mehdi.chaouche@le-campus-numerique.fr>
2020-08-28 13:44:51 +0200

    Update rapide

--- src/services.js
+++ src/services.js
@@ -68,11 +68,22 @@ export default class EventService {
      * @return {null | Event}
      */
     getLongestEvent() {
-        let events = this._eventRepository.getAll();
-        events.forEach(function (event, index) {
+        const ms_per_day = 1000 * 60 * 60 * 24;
 
+        function dateDiffInDays(a, b) {
+            const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+            const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+            return Math.floor((utc2 - utc1) / ms_per_day);
+        }
+
+        let events = this._eventRepository.getAll();
+        events.forEach(function (event) {
+            const a = event.startTime,
+                b = event.endTime;
+            console.log(dateDiffInDays(a, b))
+            return dateDiffInDays(a, b);
         });
-        return null; //TODO
     }
 
     /**
--- test/services.test.js
+++ test/services.test.js
@@ -1,10 +1,11 @@
 import Event from "../src/models";
 import EventRepository from "../src/repository";
 import EventService from "../src/services";
+
 jest.mock("../src/repository");
 
 
-describe("Event Service",()=> {
+describe("Event Service", () => {
 
     beforeEach(() => {
         // Clear all instances and calls to constructor and all methods:
@@ -17,9 +18,9 @@ describe("Event Service",()=> {
     });
 
     let fakeEvents = [
-        new Event(new Date('2019-12-17T03:24:00'),new Date('2019-12-17T13:24:00'),"Hello World","Campus Numerique","This is an hello world.."),
-        new Event(new Date('2018-12-17T03:24:00'),new Date('1995-12-17T03:24:00'),"First event","Campus Numerique","This is an hello world.."),
-        new Event(new Date('2020-04-01T09:00:00'),new Date('2020-04-01T17:00:00'),"Unit test againt","Campus Numerique","This is an hello world..")
+        new Event(new Date('2019-12-17T03:24:00'), new Date('2019-12-17T13:24:00'), "Hello World", "Campus Numerique", "This is an hello world.."),
+        new Event(new Date('2018-12-17T03:24:00'), new Date('1995-12-17T03:24:00'), "First event", "Campus Numerique", "This is an hello world.."),
+        new Event(new Date('2020-04-01T09:00:00'), new Date('2020-04-01T17:00:00'), "Unit test againt", "Campus Numerique", "This is an hello world..")
     ];
 
     test('getEvents shall call repository', async () => {
@@ -43,5 +44,8 @@ describe("Event Service",()=> {
         expect(eventService.getLastEvent()).toStrictEqual(new Date('2020-04-01T09:00:00'));
     })
 
-
+    test('getLongestEvent shall return longest result', async () => {
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getLongestEvent()).toStrictEqual();
+    })
 });
